4c6e962b159742d4412324bd5195f5b9821466bc
aground5 <aground5@naver.com>
2022-08-22 20:58:26 +0900

    database ver1

---
+++ earth_diary/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+# Create your models here.
+class Diary(models.Model) :
+    date = models.DateField()					# 날짜
+    state = models.CharField(max_length=100)	# 나라
+    content = models.TextField()				# 요약내용
+    keyword = models.CharField(max_length=100, default="")  # 키워드
+    def __str__(self) :
+        return self.state
+    
+class Image(models.Model) :
+    diary = models.ForeignKey(Diary, on_delete=models.CASCADE)
+    url = models.TextField()            # 이미지 URL 
+
+class News(models.Model) :
+    diary = models.ForeignKey(Diary, on_delete=models.CASCADE)
+    title = models.CharField(max_length=300)    # 뉴스 제목
+    url = models.TextField()                    # 뉴스 URL
+
+class Comment(models.Model) :
+    diary = models.ForeignKey(Diary, on_delete=models.CASCADE)
---
+++ earth_diary/views.py
@@ -0,0 +1,18 @@
+from datetime import date
+from django.shortcuts import render, get_object_or_404
+
+from .models import Diary
+
+# Create your views here.
+def index(request) :
+    diary_list = Diary.objects.filter(date=date.today())
+    context = {'diary_list': diary_list}
+    return render(request, 'earth_diary/diary_list.html', context)
+
+def detail(request, diary_id) :
+    diary = get_object_or_404(Diary, pk=diary_id)
+    
+    context = {
+        'diary': diary,
+    }
+    return render(request, 'earth_diary/diary_detail.html', context)
