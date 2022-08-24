6232b5f0bf9c3d576cc933bc466b883d289d765d
aground5 <aground5@naver.com>
2022-08-24 14:48:32 +0900

    devmode

--- earth_diary/urls.py
+++ earth_diary/urls.py
@@ -4,7 +4,8 @@ from . import views
 app_name = 'earth_diary'
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('candidate1', views.index, name='index'),
+    path('candidate2', views.index2, name='index2'),
     path('<int:diary_id>/', views.detail, name='detail'),
     path('test', views.test, name='test'),
 ]
--- earth_diary/views.py
+++ earth_diary/views.py
@@ -9,6 +9,11 @@ def index(request) :
     context = {'diary_list': diary_list}
     return render(request, 'earth_diary/diary_list.html', context)
 
+def index2(request) :
+    diary_list = Diary.objects.filter(date=date.today())
+    context = {'diary_list': diary_list}
+    return render(request, 'earth_diary/question_list.html', context)
+
 def detail(request, diary_id) :
     diary = get_object_or_404(Diary, pk=diary_id)
     
