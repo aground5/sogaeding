084011c80b84b3d04d20bb069a5c1de1c299ec4b
aground5 <aground5@naver.com>
2022-08-24 14:22:11 +0900

    nav_bar fixed

--- text.py
+++
@@ -1,7 +0,0 @@
-from pathlib import Path
-import os, environ
-
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
-print(BASE_DIR)
-print(type(BASE_DIR))
