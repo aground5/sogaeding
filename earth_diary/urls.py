7d2debf4f0a0a66fdfe84ca5fe8c2d5b67cc9581
aground5 <aground5@naver.com>
2022-08-25 10:36:01 +0900

    apply date changer & add card view

--- earth_diary/urls.py
+++ earth_diary/urls.py
@@ -1,11 +1,24 @@
-from django.urls import path
+from django.urls import path, register_converter
+from datetime import date as module_date
 from . import views
 
 app_name = 'earth_diary'
 
+class DateConverter:
+    regex = '\d{4}-\d{2}-\d{2}'
+
+    def to_python(self, value):
+        return module_date.fromisoformat(value)
+
+    def to_url(self, value):
+        return value
+
+register_converter(DateConverter, 'yyyy')
+
 urlpatterns = [
     path('candidate1', views.index, name='index'),
     path('candidate2', views.index2, name='index2'),
+    path('date/<yyyy:date>/', views.bydate, name='date'),
     path('<int:diary_id>/', views.detail, name='detail'),
     path('test', views.test, name='test'),
 ]
--- earth_diary/views.py
+++ earth_diary/views.py
@@ -1,18 +1,41 @@
-from datetime import date
+from datetime import date as module_date, timedelta
 from django.shortcuts import render, get_object_or_404
 
 from .models import Diary
 
 # Create your views here.
 def index(request) :
-    diary_list = Diary.objects.filter(date=date.today())
-    context = {'diary_list': diary_list}
-    return render(request, 'earth_diary/diary_list.html', context)
+    diary_list = Diary.objects.filter(date=module_date.today())
+    context = {
+        'diary_list': diary_list,
+        'prev_date': module_date.strftime(module_date.today() - timedelta(days=1), '%Y-%m-%d'),
+        'next_date': module_date.strftime(module_date.today() + timedelta(days=1), '%Y-%m-%d')
+    }
+    context['today'] = "Today"
+    return render(request, 'earth_diary/diary_list_2.html', context)
 
 def index2(request) :
-    diary_list = Diary.objects.filter(date=date.today())
+    diary_list = Diary.objects.filter(date=module_date.today())
     context = {'diary_list': diary_list}
+    context = {
+        'diary_list': diary_list,
+        'prev_date': module_date.strftime(module_date.today() - timedelta(days=1), '%Y-%m-%d'),
+        'next_date': module_date.strftime(module_date.today() + timedelta(days=1), '%Y-%m-%d')
+    }
     return render(request, 'earth_diary/question_list.html', context)
+
+def bydate(request, date) :
+    diary_list = Diary.objects.filter(date=date)
+    context = {
+        'diary_list': diary_list,
+        'prev_date': module_date.strftime(date - timedelta(days=1), '%Y-%m-%d'),
+        'next_date': module_date.strftime(date + timedelta(days=1), '%Y-%m-%d')
+    }
+    if (date == module_date.today()) :
+        context['today'] = "Today"
+    else :
+        context['today'] = module_date.strftime(date, '%Y-%m-%d')
+    return render(request, 'earth_diary/diary_list_2.html', context)
 
 def detail(request, diary_id) :
     diary = get_object_or_404(Diary, pk=diary_id)
