d4c346fcb80a7b6059e0b8a83ecb1f48ab55d9da
zynny <top6543top@naver.com>
2022-08-26 08:57:02 +0900

    난난루

--- crawling/korea.sh
+++ crawling/korea.sh
@@ -3,11 +3,11 @@ mkdir /data
 TODAY=`date "+%Y%m%d"`
 if [ `ls -l | grep $TODAY | wc -l` == 0 ]
 then 
-mkdir $TODAY
+mkdir /data/$TODAY
 fi
 echo "./naver_news.py start"
 python3 ./naver_news.py
 echo "./crawling_keyword.py start"
 python3 ./crawling_keyword.py
-echo "./crawling_pick_news.py start"
-python3 ./crawling_pick_news.py
+echo "./pick_news.py start"
+python3 ./pick_news.py
