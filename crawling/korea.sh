d1bacf556530b23193cbd925ea4a5b0139115c6d
dbtmddn41 <dbtmddn41@naver.com>
2022-08-26 10:06:50 +0900

    korea.sh

--- crawling/korea.sh
+++ crawling/korea.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cd crawling
+cd /
 if [ `ls -l | grep "data" | wc -l` == 0 ]
 then
 mkdir /data
@@ -9,20 +9,9 @@ if [ `ls -l ./data| grep $TODAY | wc -l` == 0 ]
 then 
 mkdir /data/$TODAY
 fi
-<<<<<<< HEAD
 echo "./naver_news.py start"
 python3 ./naver_news.py
 echo "./crawling_keyword.py start"
 python3 ./crawling_keyword.py
 echo "./pick_news.py start"
 python3 ./pick_news.py
-=======
-echo "/naver_news.py start"
-python3 /naver_news.py
-echo "/crawling_keyword.py start"
-python3 /crawling_keyword.py
-echo "/pick_news.py start"
-python3 /pick_news.py
-echo "/summary_article.py start"
-python3 /summary_article.py
->>>>>>> f40af947d95721e125c2e23d7a876708fc733400
